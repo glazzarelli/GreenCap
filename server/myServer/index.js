0a8ab34fe173c7447d5b077461a8f2a22d534c05
Gabriele Lazzarelli <47924888+glazzarelli@users.noreply.github.com>
2023-05-06 18:38:11 +0200

    fix database

--- server/myServer/index.js
+++ server/myServer/index.js
@@ -95,45 +95,18 @@ async function initDB() {
         },
     })
 
-    models.Dog = db.define('dog', {
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        breed: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        age: {
-            type: DataTypes.NUMBER,
-            allowNull: false
-        },
-        description: {
-            type: DataTypes.STRING,
-            allowNull: true
-        }
-    })
-
-    models.Location = db.define('location', {
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        city: {
-            type: DataTypes.STRING,
-            allowNull: false
-        }
-    })
-
+    // 
     // Relationships
-    models.Location.hasMany(models.Dog)
-    models.Dog.belongsTo(models.Location)
+    // 
 
-    models.Area.hasMany(models.Project)
-    models.Project.hasMany(models.Area)
+    // Many to many 
+    models.Area.belongsToMany(models.Project, { through: 'ProjectAreas' });
+    models.Project.belongsToMany(models.Area, { through: 'ProjectAreas' });
 
+    // One to many
     models.Project.hasOne(models.Person)
     models.Person.hasMany(models.Project)
+
 
     await db.sync({ force: true })
 
@@ -145,7 +118,9 @@ async function initDB() {
 async function initServer() {
     const models = await initDB()
 
+    //
     // API calls for project(s)
+    //
 
     app.get('/projects', async (req, res) => {
         const data = await models.Project.findAll();
@@ -177,11 +152,12 @@ async function initServer() {
         }
     })
 
+    //
     // API calls for Area(s)
+    //
 
     app.get('/areas', async (req, res) => {
         const data = await models.Area.findAll();
-
         res.status(200).json(data)
     })
 
@@ -205,7 +181,9 @@ async function initServer() {
         }
     })
 
-    // API calls for Person(s)
+    // 
+    // API calls for Person(s) 
+    // 
 
     app.get('/people', async (req, res) => {
         const data = await models.Person.findAll();
@@ -227,143 +205,6 @@ async function initServer() {
 
         if (data) {
             res.status(200).json(data)
-        }
-        else {
-            res.sendStatus(404)
-        }
-    })
-
-    app.get('/dogs', async (req, res) => {
-        const data = await models.Dog.findAll();
-
-        res.status(200).json(data)
-    })
-
-    app.get('/dogs/:id', async (req, res) => {
-        const data = await models.Dog.findOne({
-            where: {
-                id: req.params.id
-            },
-            include: [
-                {
-                    model: models.Location
-                }
-            ]
-        })
-
-        if (data) {
-            res.status(200).json(data)
-        }
-        else {
-            res.sendStatus(404)
-        }
-    })
-
-    app.post('/dogs', async (req, res) => {
-        const body = req.body;
-        if (!body.age) {
-            body.age = Math.floor(Math.random() * 14)
-        }
-
-        try {
-            body.description = loremIpsum({
-                count: 4,
-                units: "paragraph"
-            });
-            body.locationId = 1;
-
-            const record = await models.Dog.create(body)
-
-            res.status(200).json({
-                name: record.name,
-                breed: record.breed,
-                id: record.id
-            })
-        }
-        catch {
-            res.status(400).send()
-        }
-    })
-
-    app.get('/locations', async (req, res) => {
-        const data = await models.Location.findAll();
-
-        res.status(200).json(data)
-    })
-
-    app.get('/locations/:id', async (req, res) => {
-        const data = await models.Location.findOne({
-            where: {
-                id: req.params.id
-            },
-            include: [
-                {
-                    model: models.Dog
-                }
-            ]
-        })
-
-        if (data) {
-            res.status(200).json(data)
-        }
-        else {
-            res.sendStatus(404)
-        }
-    })
-
-    app.get('/dogPageSSR/:id', async (req, res) => {
-        const data = await models.Dog.findOne({
-            where: {
-                id: req.params.id
-            },
-            include: [
-                {
-                    model: models.Location
-                }
-            ]
-        })
-        if (data) {
-            let str = `
-            <!DOCTYPE html>
-            <html>
-                <head>
-                    <title>Dog Shelter - Dog Page</title>
-                    <meta name = "description" content = "Page with all the information about the shelter for dogs">
-                    <meta name = "keywords" content = "dog, shelter, information">
-                    <meta name = "author" content = "Alberto Patti">
-                    <link rel = "stylesheet" href = "/assets/css/general.css">
-                    <link rel = "stylesheet" href = "/assets/css/dogPage.css">
-                </head>
-                <body>
-                    <header>
-                        <h1>The Dog Shelter</h1>
-                        <nav>
-                            <a href = "/index.html">HOME</a>
-                            <a href = "/dogs.html">DOGS</a>
-                            <a href = "/about.html">ABOUT US</a>
-                            <a href = "/contact.html">CONTACT US</a>
-                        </nav>
-                    </header>
-                    <main>
-                        <div class = "info-group">
-                            <img id = "main-img" src = "/assets/img/home-image.jpg" />
-                            <div id = "data-container">
-                                <p class = "data">Name: <span id = "dog-name">${data.name}</span></p>
-                                <p class = "data">Breed: <span id = "dog-breed">${data.breed}</span></p>
-                                <p class = "data">Age: <span id = "dog-age">${data.age}</span></p>
-                            </div>
-                        </div>
-                        <h2>Description</h2>
-                        <p id = "description">${data.description}</p>
-                        <a id = "link" href = '/locationPage.html?id=${data.location.id}'>Link to ${data.location.name} in ${data.location.city}</a>
-                    </main>
-                    <footer>
-                        Made by me :)
-                    </footer>
-                </body>
-            </html>`;
-
-            res.status(200).send(str)
         }
         else {
             res.sendStatus(404)
