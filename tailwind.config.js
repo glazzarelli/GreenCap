9dbd85a34c027c3ae5c185130e2e9eb3e223cb39
glazzarelli <gabriele.lazzarelli@mail.polimi.it>
2023-05-12 18:14:23 +0200

    add tailwind-daisyui config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,14 @@
 module.exports = {
   plugins: [require("daisyui")],
+
+  daisyui: {
+    styled: true,
+    themes: ["lemonade", "forest"],
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "forest",
+  },
 };
