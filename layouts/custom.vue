df8027750cd20fd2b38eb7ce6aef828bbb58f51c
glazzarelli <gabriele.lazzarelli@mail.polimi.it>
2023-05-14 18:52:03 +0200

    add custom layout
    for home and error page

---
+++ layouts/custom.vue
@@ -0,0 +1,21 @@
+<!-- no margin in <main> layout, to use with hero page and error page -->
+<template>
+    <div>
+        <!-- header -->
+        <Header />
+        <!-- main -->
+        <main>
+            <slot />
+        </main>
+        <!-- footer -->
+        <Footer />
+    </div>
+</template>
+
+<script>
+
+
+</script>
+
+<style></style>
+
