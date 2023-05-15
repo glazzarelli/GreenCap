8c9ff0e31315d340ca8b3fb61af130131e4c231c
z4n0 <luca.zanotto@mail.polimi.it>
2023-05-16 00:03:01 +0200

    extracted emailValidationLogic into a composable

--- pages/contact.vue
+++ pages/contact.vue
@@ -25,15 +25,27 @@
                 <h2 class="text-lg mb-1 font-medium title-font">Feedback</h2>
                 <p class="leading-relaxed mb-5 ">Post-ironic portland shabby chic echo park, banjo fashion axe
                 </p>
-                <div class="relative mb-4">
+                <div class="relative mb-2">
                     <label for="email" class="leading-7 text-sm ">Email</label>
-                    <input type="email" id="email" name="email"
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        v-model="email"
+                        @input="validateEmail"
                         class="w-full  rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                        <p class="text-red-500 mt-2" v-if="emailError">{{ emailError }}</p>
                 </div>
-                <div class="relative mb-4">
+                <div class="relative mb-2">
                     <label for="email" class="leading-7 text-sm ">Email</label>
-                    <input type="email" id="email" name="email"
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        v-model="email"
+                        @input="validateEmail"
                         class="w-full  rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                        <p class="text-red-500 mt-2" v-if="emailError">{{ emailError }}</p>
                 </div>
                 <div class="relative mb-4">
                     <label for="message" class="leading-7 text-sm ">Message</label>
@@ -49,7 +61,8 @@
 </template>
 
 <script setup>
-
+    import useEmailValidation from '@/composables/useEmailValidation';
+    const { email, emailError } = useEmailValidation();
 </script>
 
 <style scoped></style>
