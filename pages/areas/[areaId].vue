82500a3f874d9301569230fdd2d46f774f9087e3
z4n0 <luca.zanotto@mail.polimi.it>
2023-05-17 17:02:01 +0200

    "Areas pages added"

--- pages/areas/[areaId].vue
+++ pages/areas/[areaId].vue
@@ -1,12 +1,32 @@
 <template>
   <div v-if="area">
     <Heading :title="'Areas / ' + area.name" />
-    <p> This is the passed area: {{ area.name }}</p>
+    <section class="text-gray-600 body-font">
+    <div class="container px-5 py-18 mx-auto flex flex-wrap">
+      <div class="lg:w-1/2 w-full mb-10 lg:mb-0 rounded-lg overflow-hidden">
+        <img alt="feature" class="object-cover object-center h-full w-full"  :src="'/assets/images/areas/' + area.name.replace(/ /g, '-') + '.png'">
+      </div>
+      <div class="flex flex-col flex-wrap lg:py-6 -mb-10 lg:w-1/2 lg:pl-12 lg:text-left text-center">
+        <div class="flex flex-col mb-10 lg:items-start items-center">
+          <div class="flex-grow">
+            <h2 class="text-gray-900 text-lg title-font font-medium mb-3">{{area.descriptionShort}}</h2>
+            <button class="mt-3 text-indigo-500 inline-flex items-center" @click="expand = !expand">Learn More
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                <path d="M5 12h14M12 5l7 7-7 7"></path>
+              </svg>
+            </button>
+            <p v-if="expand" class="mt-3 text-gray-500">{{area.descriptionLong}}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
   </div>
   <p v-else>Loading...</p>
 </template>
 
 <script setup>
+const expand = ref(false);
 const { areaId } = useRoute().params;
 console.log('Area id: ', areaId);
 //{ data: area } is object destructuring syntax that extracts the data property from the response object
@@ -16,4 +36,5 @@ if(!area.value){
    throw createError({statusCode: 404, statusMessage: 'Area not found', fatal: true});
  }
 
+//const path = area.name.replace(' ', '-');
 </script>
--- pages/contact.vue
+++ pages/contact.vue
@@ -36,17 +36,6 @@
                         class="w-full  rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                         <p class="text-red-500 mt-2" v-if="emailError">{{ emailError }}</p>
                 </div>
-                <div class="relative mb-2">
-                    <label for="email" class="leading-7 text-sm ">Email</label>
-                    <input
-                        type="email"
-                        id="email"
-                        name="email"
-                        v-model="email"
-                        @input="validateEmail"
-                        class="w-full  rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
-                        <p class="text-red-500 mt-2" v-if="emailError">{{ emailError }}</p>
-                </div>
                 <div class="relative mb-4">
                     <label for="message" class="leading-7 text-sm ">Message</label>
                     <textarea id="message" name="message"
