2b4cc8722cb370fe483d3e84f16a4f17d1484b1a
z4n0 <luca.zanotto@mail.polimi.it>
2023-05-18 10:11:32 +0200

    change names

--- components/PersonCard.vue
+++ components/PersonCard.vue
@@ -2,16 +2,19 @@
   <div class="relative">
     <nuxt-link :to="`/people/${person.id}`">
       <div class="card card-compact image-full max-w-md mx-auto">
-        <figure> <img :src="`/images/people/${person.image}`" />
+        <figure class="relative"> 
+          <img :src="`/images/people/${person.image}`" />
+          <figcaption class="absolute bottom-0 w-full bg-black bg-opacity-75 text-white text-center p-2">
+            <h2 class="card-title justify-center font-semibold text-shadow">{{ person.name }} {{ person.surname }}</h2>
+          </figcaption>
         </figure>
         <div class="card-body text-center mt-28">
-          <h2 class="card-title justify-center">{{ person.name }} {{ person.surname }}</h2>
-          <p class="neutral">Supervisor</p>
         </div>
       </div>
     </nuxt-link>
   </div>
 </template>
+
   
 <script setup>
 const { person } = defineProps({
@@ -22,4 +25,10 @@ const { person } = defineProps({
 });
     //console.log("this is the person data: ", person);
 </script>
+
+<style scoped>
+figcaption h2 {
+    text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
+}
+</style>
   
