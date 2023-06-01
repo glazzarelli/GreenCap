720033c0e2a5bb2963730ed0c43ec1b9751817ee
Martina Del Basso <102237556+martidb2000@users.noreply.github.com>
2023-06-01 22:03:11 +0200

    footer accessibility

--- components/Footer.vue
+++ components/Footer.vue
@@ -3,7 +3,7 @@
     class="footer p-5 sm:p-10 bg-base-200 text-base-content max-sm:justify-center items-center max-sm:grid-flow-row max-sm:justify-items-center border-t border-[primary]">
     <div class="flex flex-col sm:flex-row max-sm:grid-flow-row " >
       <div class="mx-2 max-sm:grid-flow-row ">
-        <span class="footer-title mx-1 font-bold text-lg" style="color: #000; opacity: 1;">Newsletter</span>
+        <span class="footer-title mx-1 font-bold text-lg" style=" opacity: 1;">Newsletter</span>
         <label class="label">
           <span class="label-text">Enter your email address</span>
         </label>
@@ -11,12 +11,12 @@
       <div class="form-control w-80">
         <div class="relative">
           <input type="text" v-model="email" placeholder="username@site.com" class="input input-bordered w-full pr-16" />
-          <button class="btn btn-secondary absolute top-0 right-0 rounded-l-none">Subscribe</button>
+          <button class="btn btn-secondary absolute top-0 right-0 rounded-l-none" style=" opacity: 1;">Subscribe</button>
         </div>
         <p class="text-red-500 mt-2" v-if="emailError">{{ emailError }}</p>
       </div>
     </div>
-    <div class="items-center footer-end grid-flow-col" style="color: #000;" >
+    <div class="items-center footer-end grid-flow-col" >
       <IconLogo class="h-7" />
       <p>Copyright © 2023 GreenCap - All right reserved</p>
     </div>
@@ -37,6 +37,6 @@ const { email, emailError } = useEmailValidation();
 .btn,
 .footer-end p {
   font-weight: bold;
-  color: #000;
+  
 }
 </style>
