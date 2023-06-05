312442049df086b0eae5ba72bc5a9bc7b8d9ff73
z4n0 <luca.zanotto@mail.polimi.it>
2023-06-05 19:24:01 +0200

    Updates

--- _nuxt/Heading.4726ff41.js
+++
@@ -1,1 +0,0 @@
-import{o as t,c as s,a,t as c}from"./entry.a28e24fd.js";const n={class:"flex items-center justify-center"},l={__name:"Heading",props:{title:String},setup(e){return(r,o)=>(t(),s("div",n,[a("h1",null,c(e.title),1)]))}};export{l as _};
