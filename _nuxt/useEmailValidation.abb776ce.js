f20a48144f20577e7d6dabc797497132b149014e
z4n0 <luca.zanotto@mail.polimi.it>
2023-06-06 16:17:29 +0200

    Updates

---
+++ _nuxt/useEmailValidation.abb776ce.js
@@ -0,0 +1,1 @@
+import{r as t,E as i}from"./entry.a31b599d.js";function l(){const e=t(""),a=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,r=i(()=>e.value===""||a.test(e.value)?"":"Please enter a valid email address");return{email:e,emailError:r}}export{l as u};
