de8c2133a40c681e701ac46f402b9fe7d8b1d125
z4n0 <luca.zanotto@mail.polimi.it>
2023-06-07 15:15:38 +0200

    Updates

--- _nuxt/useEmailValidation.36659eec.js
+++
@@ -1,1 +0,0 @@
-import{r as t,E as i}from"./entry.243a4834.js";function l(){const e=t(""),a=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,r=i(()=>e.value===""||a.test(e.value)?"":"Please enter a valid email address");return{email:e,emailError:r}}export{l as u};
