5d0cfc4d4610d055640c0bdd45d5c15a7603d4b7
z4n0 <luca.zanotto@mail.polimi.it>
2023-06-08 16:59:43 +0200

    removed comments

--- FrontEnd/pages/people/[personId].vue
+++ FrontEnd/pages/people/[personId].vue
@@ -19,23 +19,6 @@
     </div>
   </section>
 </template>
-
-  <!-- <template>
-    <Heading :title="`People / ` + person.name + ' ' + person.surname" />
-    <div class="mt-10 mb-18">
-      <img :src=imagePath class="rounded-md lg:float-left lg:h-1/3 lg:w-1/3 mr-8 mb-2 shadow-lg lg:max-h-screen lg:object-cover" alt="Person Image">
-      <p class="italic text-center text-4xl font-serif pl-6 pr-6 mb-8">"{{person.motto}}"</p>
-      <h2>{{ person.cv }}</h2>
-    </div>
-    <div class="mt-12">
-      <h1>Supervised Projects</h1>
-      <div class="card-container">
-        <ProjectCardSmall v-for="project in person.projects" :key="project.id" :project="project"
-          class="w-full sm:w-1/2 lg:w-1/3 xl:w-1/4 p-2" />
-      </div>
-    </div>
-  </template> -->
-
 <script setup>
 import useImages from '@/composables/useImages';
 
