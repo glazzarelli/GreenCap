6be751f51a759e74aed6848f4367bf5a0d95654f
z4n0 <luca.zanotto@mail.polimi.it>
2023-06-13 18:49:30 +0200

    renaming and animation fix

--- FrontEnd/components/ChartSection.vue
+++ FrontEnd/components/ChartSection.vue
@@ -1,20 +1,19 @@
-
 <template>
   <section class="py-12 bg-primary-content px-4">
-    <div class="max-w-7xl mx-auto">
+    <div class="max-w-7xl mx-auto opacity-0" :class="{ 'slide-in-right': isTextInView }">
       <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
         <!-- The left column with a title, a paragraph and an animation -->
-        <div class="py-2 mt-2" :class="{ 'slide-in-right': isTextInView }">
-          <h2 class="text-3xl font-semibold mb-6 slide-in">
+        <div class="py-2 mt-2">
+          <h2 class="text-3xl font-semibold mb-6">
             Why choose GreenCapital
           </h2>
           <p class="text-lg my-auto" ref="textParagraph">
-            {{ aboutData.description }}
+            We are a venture capital firm focused on helping startups grow and succeed. Our team brings decades of experience in various industries, and we are passionate about supporting the next generation of entrepreneurs.
           </p>
         </div>
         <!-- The right column with a title, a chart and some margin -->
         <div>
-          <h2 class="text-3xl lg:mt-4 lg:ml-20 font-semibold mb-6 slide-in">Our investments so far</h2>
+          <h2 class="text-3xl lg:mt-4 lg:ml-20 font-semibold mb-6">Our investments so far</h2>
           <div class="mt-4 mb-18">
             <Chart />
           </div>
@@ -26,12 +25,6 @@
 
 <script setup>
 import Chart from '@/components/Chart.vue';
-
-// Data for the about section
-const aboutData = {
-  description: "We are a venture capital firm focused on helping startups grow and succeed. Our team brings decades of experience in various industries, and we are passionate about supporting the next generation of entrepreneurs.",
-  image: "images/other/chart3.png" // TODO remove this if not used
-}
 
 // Refs  variables for the animation
 const textParagraph = ref(null);
@@ -67,7 +60,6 @@ onBeforeUnmount(() => {
 @keyframes slide-in-right {
   0% {
     transform: translateX(100%);
-    opacity: 0;
   }
 
   100% {
@@ -76,21 +68,7 @@ onBeforeUnmount(() => {
   }
 }
 
-@keyframes fade-in {
-  0% {
-    opacity: 0;
-  }
-
-  100% {
-    opacity: 1;
-  }
-}
-
 .slide-in-right {
   animation: slide-in-right 2s forwards;
-}
-
-.fade-in {
-  animation: fade-in 2s forwards;
 }
 </style>
